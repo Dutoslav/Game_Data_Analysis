5993dabb613db547956ad6eafab149128cd1eeb0
Dutoslav <106549354+Dutoslav@users.noreply.github.com>
2022-11-14 17:05:43 +0100

    Add files via upload

---
+++ data_analysis.py
@@ -0,0 +1,71 @@
+#data_analysis.py analyzes the randomly generated data from game_generator.py#
+import time
+startTime = time.time()
+import json
+import game_generator   #Importing the game_generator module in order to access its generate_game_data() function#
+
+analyzed_data = {'Release' : {}, 'Platforms' : {}, 'Online' : {}, 'Games per Developer' : {}, 'Average Sales per Developer' : {}} #Dictionary where final results are stored#
+sales_per_developer = {}  #Dictionary for sales per developer that are needed in the average_sales() function#
+i = 0  #Iterator for the main data analysis block of code#
+
+def average_sales():   #This function calculates the average number of sales per game for each Developer and stores the results in the analyzed_data dictionary#
+  for key, value in sales_per_developer.items():
+    analyzed_data['Average Sales per Developer'][key] = sales_per_developer[key] / analyzed_data['Games per Developer'][key]
+
+def data_analysis():   #This function analyzes data in the game_data dictionary and stores the results in the analyzed_data dictionary#
+  for key, value in game_data.items():
+    if key == 'Release Year':
+      if str(value) in analyzed_data['Release']:
+        analyzed_data['Release'][str(value)] += 1
+      else:
+        analyzed_data['Release'][str(value)] = 1
+    
+    elif key == 'Game Platform(s)':
+      for platform in value:
+        if platform in analyzed_data['Platforms']:
+          analyzed_data['Platforms'][platform] += 1
+        else:
+          analyzed_data['Platforms'][platform] = 1
+    
+    elif key == 'Is Online':
+      if value in analyzed_data['Online']:
+        analyzed_data['Online'][value] += 1
+      else:
+        analyzed_data['Online'][value] = 1
+
+    elif key == 'Developer':
+      if value in analyzed_data['Games per Developer']:
+        analyzed_data['Games per Developer'][value] += 1
+      else:
+        analyzed_data['Games per Developer'][value] = 1
+      
+      if value in sales_per_developer:
+        sales_per_developer[value] += game_data['Sales']
+      else:
+        sales_per_developer[value] = game_data['Sales']
+
+
+while i < 10000:   #Main block of code, opens the game_info.json file, stores the dictionary in game_data, generates the random game data through game_generator.generate_game_data() and runs the data_analysis() function#
+  game_generator.generate_game_data()
+  with open('game_info.json') as game_json:
+    game_data = json.load(game_json)
+
+  print(game_data)
+  data_analysis()
+  i += 1
+  
+average_sales()
+
+for key, value in analyzed_data.items():  #This part of code prints out the results of the anlysis from the analyzed_data dictionary#
+  print(str(key) + ':\n')
+  if key == 'Release':
+    years = analyzed_data['Release']
+    sorted_years = sorted(years.items())
+    sorted_years_dict = {k:v for k, v in sorted_years}
+    for inner_key, inner_value in sorted_years_dict.items():
+      print(' ' + str(inner_key) + ': ' + str(inner_value) + '\n')
+  else:
+    for inner_key, inner_value in value.items():
+      print(' ' + str(inner_key) + ': ' + str(inner_value) + '\n')
+  
+print('Final script time in seconds: ' + str(time.time() - startTime))  #This is just so I know how inefficient my code is#
---
+++ game_generator.py
@@ -0,0 +1,68 @@
+#game_generator.py generates random game data for data_analysis.py#
+import json
+import random
+
+write_to_json = {}
+
+def game_name_generator():
+    first_name_part = ['Mega', 'Party', 'Ultimate', 'Complete', '', 'Beast', 'Hyper', 'Fearless']
+    second_name_part = [' Cycling', ' Outer', ' Racing', ' Fighting', ' Survival', ' Human', ' Viking', ' Sword']
+    third_name_part = [' : 2', ' : 3', ' Endless Edition', ' Game', '', ' Legend', ' Complete Edition', ' GOTY']
+
+    write_to_json['Game Name'] = '{first}{second}{third}'.format(first = first_name_part[random.randrange(0, 8)], second = second_name_part[random.randrange(0, 8)], third = third_name_part[random.randrange(0, 8)])
+    
+def game_release_generator():
+    write_to_json['Release Year'] = random.randrange(1990, 2022) 
+
+def sales_number_generator():
+    weight = random.randrange(1, 100)
+    if weight <= 5:
+        write_to_json['Sales'] = random.randrange(8000000, 12000000)
+    elif weight <= 15:
+        write_to_json['Sales'] = random.randrange(4000000, 7999999)
+    elif weight <= 40:
+        write_to_json['Sales'] = random.randrange(1000000, 3999999)
+    else:
+        write_to_json['Sales'] = random.randrange(5000, 999999)
+
+def platform_generator():
+    platforms = ['PC', 'Mac', 'PS', 'XBox', 'Linux', 'Mobile']
+    platforms_to_add = []
+    num_platforms = 5
+    i = 0
+    platforms_to_generate = random.randrange(1, 6)
+
+    while i < platforms_to_generate:
+        if num_platforms != 0:
+            add_platform = platforms[random.randrange(0, num_platforms)]
+        else:
+            add_platform = platforms[0]
+
+        platforms_to_add.append(add_platform)
+        platforms.remove(add_platform)
+        num_platforms -= 1
+
+        i += 1
+    write_to_json['Game Platform(s)'] = platforms_to_add
+
+def is_online_generator():
+    if random.randrange(1, 100) < 30:
+        write_to_json['Is Online'] = True
+    else:
+        write_to_json['Is Online'] = False
+
+def developer_generator():
+    developers_list = ['Activision', 'EA', 'Blizzard', 'Bethesda', 'Obsidian', 'Valve', '3D', 'Firework', 'Ubisoft', 'Octopie', 'RockSolid', 'CD Project Red', 'Paradox']
+    write_to_json['Developer'] = developers_list[random.randrange(0, 12)]
+
+def generate_game_data():
+
+    game_name_generator()
+    game_release_generator()
+    sales_number_generator()
+    platform_generator()
+    is_online_generator()
+    developer_generator()
+
+    with open('game_info.json', 'w') as json_file:
+        json.dump(write_to_json, json_file)
